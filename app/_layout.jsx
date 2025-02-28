32b0e00c2cf498ca472c6253daff5b788ea3460a
Maxwellsmall <maxwellsmall88@gmail.com>
2025-02-28 13:01:42 +0100

    have done the first page after the splash

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,31 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { StatusBar } from "react-native";
+import { View, Image, Text } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar barStyle={"dark-content"} />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: true,
+            headerShadowVisible: false,
+            headerTitle: "",
+            headerLeft: () => (
+              <View className="flex-row justify-center items-center">
+                <Image
+                  source={require("../assets/images/logo.png")}
+                  className="w-12 h-12 mr-3"
+                />
+                <Text className="font-bold">Charmpay</Text>
+              </View>
+            ),
+          }}
+        />
+      </Stack>
+    </>
+  );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Fontisto } from "@expo/vector-icons";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-blue-500">
-      <Text className="text-white text-lg font-bold">
-        Hello, Tailwind in React Native! 🚀
+    <View className="flex-1 justify-center items-center bg-white">
+      <View>
+        <TouchableOpacity className="bg-blue-900  w-80 p-3 rounded-lg">
+          <Text className="text-white text-center font-semibold">LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="bg-[#F5F5F5] mt-4 w-80 p-3 rounded-lg">
+          <Text className="text-black text-center font-semibold">SIGNUP</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity className="flex-row justify-center items-center mt-60 absolute bottom-20 bg-[#F5F5F5] p-3 px-3 rounded-[25px]">
+        <Fontisto name="world-o" size={15} color="blue" />
+        <Text className="ms-[13px] text-blue-700 text-xs">ENGLISH</Text>
+      </TouchableOpacity>
+      <Text className="mt-60 absolute bottom-9 text-blue-900 font-bold">
+        Continue to agree to privacy policy
       </Text>
     </View>
   );
