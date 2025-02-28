f1dee504950036bb6f2af23c39e673bb39ebd5b5
Promise Ibeh <ibehpromise3d@gmail.com>
2025-02-28 09:57:45 +0100

    added tailwind css to the project

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-white text-lg font-bold">
+        Hello, Tailwind in React Native! 🚀
+      </Text>
+    </View>
+  );
+}
